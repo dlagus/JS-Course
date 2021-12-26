6c5f1c84484184c08aa8c8f7d8663e670e6a767b
dlagus <daniel.lagus@klurateknik.se>
2021-12-26 13:22:57 +0100

    Extrs check in, synq to Mac.

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,16 @@
+// LECTURE: Values and Variables
+let country = 'Sweden';
+let continent = 'Europe';
+let population = 9;
+console.log('I live in ' + country + '. ' + country + ' is part of ' + continent + '. ' + population + ' million people lives in my country.');
+
+// LECTURE: Data Types
+let isIsland = false;
+let consoleLogText = country + ' is';
+if (isIsland == false) consoleLogText = consoleLogText + ' not';
+consoleLogText = consoleLogText + ' part of an island.';
+console.log(consoleLogText);
+
+// LECTURE: let, const and var 
+let language = "Swedish";
+console.log('The prefered language in my country is ')
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,2 @@
 let js = 'amazing';
-if (js == 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
